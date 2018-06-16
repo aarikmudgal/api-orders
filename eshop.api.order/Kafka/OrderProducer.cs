2f62df66cee2f0ce4e34ca1bf518c31f1f00de17
Aarik Mudgal <aarikmudgal@gmail.com>
2018-06-16 16:12:30 +0200

    Update OrderProducer.cs

--- eshop.api.order/Kafka/OrderProducer.cs
+++ eshop.api.order/Kafka/OrderProducer.cs
@@ -15,7 +15,7 @@ namespace eshop.api.order.Kafka
         {
             var config = new Dictionary<string, object>
             {
-                {"bootstrap.servers", "35.200.177.207:9092" },
+                {"bootstrap.servers", "35.200.201.3:9092" },
                 {"debug", "cgrp" }
             };
             try
